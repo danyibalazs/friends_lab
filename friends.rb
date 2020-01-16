15ddcb0c0708efbf07039874c76e5ad07301bfb9
djghis <djghis@hotmail.com>
2020-01-16 14:29:49 +0000

    4 tests pass

--- friends.rb
+++ friends.rb
@@ -1,3 +1,15 @@
 def get_name(person)
   return person[:name]
 end
+
+def favourite_tv_show(person)
+  return person[:favourites][:tv_show]
+end
+
+def likes_to_eat(person, food)
+  return person[:favourites][:snacks].include?(food)
+end
+
+def add_friend(person, new_friend)
+  person[:friends].push(new_friend)
+end
